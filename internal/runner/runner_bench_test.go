45c328ad469f76843169627182078d022feaf679
3mar-baha <3mar-baha@users.noreply.github.com>
2026-09-10 14:33:00 +0300

    test(runner): add session path benchmarks

--- internal/runner/runner_bench_test.go
+++ internal/runner/runner_bench_test.go
@@ -13,3 +13,10 @@ func BenchmarkBuildCommand(b *testing.B) {
 		_, _ = BuildCommand(r, "openai/gpt-5.6", "max", "C:\\tmp\\x")
 	}
 }
+
+func BenchmarkTruncPath(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = TruncPath("C:\\very\\long\\path\\to\\project\\folder\\name", 20)
+	}
+}
