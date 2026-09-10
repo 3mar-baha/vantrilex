a823eb236a026e7aa7fc3e30599cedb5f66048be
3mar-baha <3mar-baha@users.noreply.github.com>
2026-09-10 14:24:00 +0300

    test(scaffold): add plan and bridge benchmarks

---
+++ internal/scaffold/scaffold_bench_test.go
@@ -0,0 +1,18 @@
+package scaffold
+
+import "testing"
+
+func BenchmarkPlan(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = Plan()
+	}
+}
+
+func BenchmarkBridgeRelPath(b *testing.B) {
+	t := Toolkit{Slug: "vercel-skills", Name: "vercel-labs/skills"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = BridgeRelPath(t)
+	}
+}
