bd6f9db5b527bb563e77f5ff4c4ae4c6a0f1ae21
3mar-baha <3mar-baha@users.noreply.github.com>
2026-09-10 14:06:00 +0300

    test(doctor): add status pill benchmarks

---
+++ internal/doctor/doctor_bench_test.go
@@ -0,0 +1,11 @@
+package doctor
+
+import "testing"
+
+func BenchmarkPill(b *testing.B) {
+	s := RunnerUpdateStatus{Updating: false, Done: true}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = s.Pill()
+	}
+}
