a6ea9a5a54e0e8240654895bb1c77d7df6b83d92
3mar-baha <3mar-baha@users.noreply.github.com>
2026-09-10 11:15:00 +0300

    test(particles): add trail and perimeter benchmarks

--- internal/particles/particles_bench_test.go
+++ internal/particles/particles_bench_test.go
@@ -20,3 +20,19 @@ func BenchmarkRenderField(b *testing.B) {
 		_ = e.RenderField()
 	}
 }
+
+func BenchmarkEmitTrail(b *testing.B) {
+	e := New(100, 24)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		e.EmitTrail(50, 12)
+	}
+}
+
+func BenchmarkPerimeterSupernova(b *testing.B) {
+	e := New(100, 24)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		e.PerimeterSupernova()
+	}
+}
